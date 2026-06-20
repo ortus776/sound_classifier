464d02ed2d16aa8863914c12f26f45a018d6e986
ortus <rush.b.fast113@gmail.com>
2026-06-20 15:14:53 +0300

    minor changes

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-from model_def import MiniVGGClassifier
-model = MiniVGGClassifier()
-model.eval()
-dummy_input = torch.randn(1, 2, 64, 345)
-torch.onnx.export(model, dummy_input, "model.onnx")
